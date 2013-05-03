bc295fbaf3f307b46659f05d032dd49b34eb38d9
Steve <stevejb71@yahoo.co.uk>
2013-05-03 23:19:47 +0800

    Diagonal conflict detection

--- src/main/scala/simann/EightQueens.scala
+++ src/main/scala/simann/EightQueens.scala
@@ -3,13 +3,25 @@ package simann
 import scalaz.effect.{IO, SafeApp}
 import scalaz.effect.IO.putStrLn
 import scalaz.syntax.equal._
+import scalaz.syntax.std.boolean._
+import scalaz.syntax.std.option._
+import scalaz.std.list._
 import scalaz.std.anyVal._
 import util.Random
 import annotation.tailrec
+import scalaz.std.stream._
 
 case class Board(indicesAtRow: List[Int]) extends AnyVal {
   def size = indicesAtRow.size
   def swap(x: Int, y: Int) = Board(indicesAtRow.updated(x, indicesAtRow(y)).updated(y, indicesAtRow(x)))
+  def countDiagonalConflicts = indicesAtRow.zipWithIndex.map{case (r, c) => countConflictsToNorthWest(r, c) + countConflictsToSouthWest(r, c)}.sum
+  private def countConflictsToNorthWest(row: Int, col: Int) = countConflictsToWest(row, col, -1)
+  private def countConflictsToSouthWest(row: Int, col: Int) = countConflictsToWest(row, col, 1)
+  private def countConflictsToWest(row: Int, col: Int, dy: Int): Int = {
+    val diagonalPositions = unfold((row, col)){case (r, c) => (r >= 0 && c >= 0 && r < size) ? ((r, c), (r + dy, c - 1)).some | None}.toList
+    diagonalPositions.isEmpty ? 0 | diagonalPositions.tail.count{case (r, c) => hasQueen(r, c)}
+  }
+  private def hasQueen(row: Int, col: Int) = indicesAtRow(row) === col
   override def toString = {
     val blankRow = List.fill(size)('.')
     val rows = indicesAtRow.toList.map(blankRow.updated(_, 'Q'))
@@ -37,6 +49,8 @@ object EightQueens extends SafeApp {
   private def solveBoard(board: Board) = ???
 
   private[simann] def initialBoard(size: Int, random: Random): Board = (1 to size).foldLeft(Board.clean(size))((b, _) => tweakBoard(b, random))
+
+  private[simann] def computeEnergy(board: Board): Int = ???
 
   @tailrec
   private def tweakBoard(board: Board, random: Random): Board = {
--- src/test/scala/simann/BoardSpec.scala
+++ src/test/scala/simann/BoardSpec.scala
@@ -13,4 +13,8 @@ class BoardSpec extends Specification {
   "Queens can be swapped" in {
     Board(2, 0, 1).swap(0, 2) must be_=== (Board(1, 0, 2))
   }
+
+  "Conflicts can be detected" in {
+    Board(1, 2, 3, 0).countDiagonalConflicts must be_=== (3)
+  }
 }
