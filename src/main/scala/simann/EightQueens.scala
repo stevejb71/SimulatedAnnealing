e5c539e6427288c45e5ffbda6958737ef707c5a3
Steve <stevejb71@yahoo.co.uk>
2013-05-04 23:20:00 +0800

    Remove more vars

--- src/main/scala/simann/EightQueens.scala
+++ src/main/scala/simann/EightQueens.scala
@@ -8,18 +8,30 @@ import annotation.tailrec
 
 case class Board(indicesAtRow: List[Int]) extends AnyVal {
   def size = indicesAtRow.size
+
   def swap(x: Int, y: Int) = Board(indicesAtRow.updated(x, indicesAtRow(y)).updated(y, indicesAtRow(x)))
-  def countDiagonalConflicts = indicesAtRow.zipWithIndex.map{case (c, r) => countConflictsToWest(r, c, -1) + countConflictsToWest(r, c, 1)}.sum
+
+  def countDiagonalConflicts = indicesAtRow.zipWithIndex.map {
+    case (c, r) => countConflictsToWest(r, c, -1) + countConflictsToWest(r, c, 1)
+  }.sum
+
   private def countConflictsToWest(row: Int, col: Int, dy: Int): Int = {
-    val diagonalPositions = unfold((row, col)){case (r, c) => (r >= 0 && c >= 0 && r < size) ?? ((r, c), (r + dy, c - 1)).some}.toList
-    diagonalPositions.isEmpty ? 0 | diagonalPositions.tail.count{case (r, c) => hasQueen(r, c)}
+    val diagonalPositions = unfold((row, col)) {
+      case (r, c) => (r >= 0 && c >= 0 && r < size) ?? ((r, c), (r + dy, c - 1)).some
+    }.toList
+    diagonalPositions.isEmpty ? 0 | diagonalPositions.tail.count {
+      case (r, c) => hasQueen(r, c)
+    }
   }
+
   private def hasQueen(row: Int, col: Int) = indicesAtRow(row) === col
 }
 
 object Board {
   def apply(qs: Int*): Board = Board(qs.toList)
+
   def clean(size: Int) = Board((0 until size).toList)
+
   implicit val showBoard = Show.shows((b: Board) => {
     val blankRow = List.fill(b.size)('.')
     val rows = b.indicesAtRow.toList.map(blankRow.updated(_, 'Q'))
@@ -45,31 +57,29 @@ object EightQueens extends SafeApp {
   }
 
   def solveBoard(board: Board, random: Random): Option[Board] = {
-    var bestBoard: Option[Board] = None
     var currentBoard = board
     val temperatures = unfold(initialTemperature)(t => (t > finalTemperature) ?? (t, (t * alpha)).some)
-    temperatures.foreach {temperature =>
-      (0 until stepsPerChange).foreach {_ =>
-        val workingBoard = tweakBoard(currentBoard, random)
-        val workingEnergy = workingBoard.countDiagonalConflicts
-        val currentEnergy = currentBoard.countDiagonalConflicts
-        val useNew = if(workingEnergy < currentEnergy) {
-          true
-        } else {
-          val test = random.nextDouble()
-          val delta = workingEnergy - currentEnergy
-          val calc = math.exp(-delta / temperature)
-          calc > test
+    val trialBoards = temperatures.map { temperature => {
+        (0 until stepsPerChange).foreach { _ =>
+          val workingBoard = tweakBoard(currentBoard, random)
+            val workingEnergy = workingBoard.countDiagonalConflicts
+            val currentEnergy = currentBoard.countDiagonalConflicts
+            val useNew = if (workingEnergy < currentEnergy) {
+              true
+            } else {
+              val test = random.nextDouble()
+              val delta = workingEnergy - currentEnergy
+              val calc = math.exp(-delta / temperature)
+              calc > test
+            }
+            if (useNew) {
+              currentBoard = workingBoard
+            }
         }
-        if(useNew) {
-          currentBoard = workingBoard
-          if(currentBoard.countDiagonalConflicts < bestBoard.map(_.countDiagonalConflicts).getOrElse(10000000)) {
-            bestBoard = currentBoard.some
-          }
-        }
+        currentBoard
       }
     }
-    bestBoard
+    trialBoards.find(_.countDiagonalConflicts === 0)
   }
 
   private[simann] def initialBoard(size: Int, random: Random): Board = (1 to size).foldLeft(Board.clean(size))((b, _) => tweakBoard(b, random))
@@ -79,7 +89,7 @@ object EightQueens extends SafeApp {
     // TODO: not functional
     val x = random.nextInt(board.size - 1)
     val y = random.nextInt(board.size - 1)
-    if(x === y) {
+    if (x === y) {
       tweakBoard(board, random)
     } else {
       board.swap(x, y)
--- src/test/scala/simann/IntegrationSpec.scala
+++ src/test/scala/simann/IntegrationSpec.scala
@@ -6,11 +6,11 @@ import scalaz.syntax.std.option._
 
 class IntegrationSpec extends Specification {
   "Solve an 8 level board" in {
-    produceSolutionAndCheck(8, 100, Board(5, 3, 6, 0, 2, 4, 1, 7))
+    produceSolutionAndCheck(8, 100, Board(3, 6, 4, 1, 5, 0, 2, 7))
   }
 
   "Solve a 10 level board" in {
-    produceSolutionAndCheck(10, 100, Board(7, 3, 8, 6, 2, 0, 5, 1, 4, 9))
+    produceSolutionAndCheck(10, 100, Board(2, 7, 5, 0, 8, 1, 4, 6, 3, 9))
   }
 
   private def produceSolutionAndCheck(boardSize: Int, random: Int, expected: Board) {
