b086e79e9d213c4e1629313e0d247c3e521bb39e
Steve <stevejb71@yahoo.co.uk>
2013-05-05 22:45:09 +0800

    Changes to Anneal, split out next function from foldLeft

--- src/main/scala/simann/Annealing.scala
+++ src/main/scala/simann/Annealing.scala
@@ -15,22 +15,21 @@ object Annealing {
     val annealable = implicitly[Annealable[A]]
     val temperatures = unfold(config.initialTemperature)(t => (t > config.finalTemperature) ?? (t, (t * config.alpha)).some)
     val trialSolutions = temperatures.map { temperature => {
-      def next(current: A) = {
-        val tweaked = annealable.tweak(current, random)
-        val delta = annealable.energy(tweaked) - annealable.energy(current)
-        if (delta < 0) {
-          tweaked
-        } else {
-          val test = random.nextDouble()
-          val calc = math.exp(-delta / temperature)
-          (calc > test) ? tweaked | current
-        }
-      }
-
-      (0 until config.stepsPerChange).foldLeft(start){case (a, _) => next(a)}
+      (0 until config.stepsPerChange).foldLeft(start){case (a, _) => next(a, temperature, random)}
     }}
     trialSolutions.find(a => annealable.energy(a) === 0)
   }
 
-
+  def next[A: Annealable](current: A, temperature: Double, random: Random) = {
+    val annealable = implicitly[Annealable[A]]
+    val tweaked = annealable.tweak(current, random)
+    val delta = annealable.energy(tweaked) - annealable.energy(current)
+    if (delta < 0) {
+      tweaked
+    } else {
+      val test = random.nextDouble()
+      val calc = math.exp(-delta / temperature)
+      (calc > test) ? tweaked | current
+    }
+  }
 }
---
+++ src/test/scala/simann/AnnealingSpec.scala
@@ -0,0 +1,8 @@
+package simann
+
+import org.specs2.mutable._
+
+class AnnealingSpec extends Specification {
+
+
+}
