4c27a4a4f1903e9df7d23463655d00a275c80094
Steve <stevejb71@yahoo.co.uk>
2013-05-05 22:57:00 +0800

    Add test on step in annealing

--- src/test/scala/simann/AnnealingSpec.scala
+++ src/test/scala/simann/AnnealingSpec.scala
@@ -1,8 +1,18 @@
 package simann
 
 import org.specs2.mutable._
+import util.Random
 
 class AnnealingSpec extends Specification {
+  "next returns tweaked if it's better than current" in {
+    val acceptanceProbability = (x: Double) => x
+    val random = new Random(100)
+    val annealable = new Annealable[Int] {
+      def tweak(a: Int, r: Random): Int = 50
+      def energy(a: Int): Double = a * 10.0
+    }
 
+    Annealing.oneStep(100, acceptanceProbability, random, annealable) must be_=== (50)
+  }
 
 }
