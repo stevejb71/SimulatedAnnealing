540011e93886ce83d4d155e302aab0d26d5752f5
Steve <stevejb71@yahoo.co.uk>
2013-05-12 17:31:26 +0800

    Simplify a little

--- src/main/scala/simann/Annealing.scala
+++ src/main/scala/simann/Annealing.scala
@@ -16,29 +16,30 @@ case class AnnealingConfig(initialTemperature: Temperature, finalTemperature: Te
 
 object Annealing {
   def anneal[A: Annealable](start: A, config: AnnealingConfig): State[Random, Option[A]] = {
-    val annealable = implicitly[Annealable[A]]
     val temperatures = unfold(config.initialTemperature)(t => (t.value > config.finalTemperature.value) ? (t, (t * config.temperatureDropRatio)).some | None)
-    def acceptanceProbability(temperature: Temperature) = (d: Double) => math.exp(-d / temperature.value)
-    def chooseTrialOrCurrent(trialSolution: A, current: A, temperature: Temperature): State[Random, A] = {
-      val threshold = Random.nextDouble
-      threshold.map(th => choose(trialSolution, current, acceptanceProbability(temperature), th, annealable.energy _))
-    }
-    anneal(start, temperatures, chooseTrialOrCurrent _, config.stepsAtEachTemperature)
+    anneal(start, temperatures, config.stepsAtEachTemperature)
   }
 
-  def anneal[A: Annealable](start: A, temperatures: Stream[Temperature], chooseTrialOrCurrent: (A, A, Temperature) => State[Random, A], stepsAtEachTemperature: Int): State[Random, Option[A]] = {
+  def anneal[A: Annealable](start: A, temperatures: Stream[Temperature], stepsAtEachTemperature: Int): State[Random, Option[A]] = {
     val trialSolutions: List[State[Random, A]] = temperatures.map { temperature =>
-      (0 until stepsAtEachTemperature).toList.foldLeftM[({type M[B] = State[Random, B]})#M, A](start){case (current, _) => trySolution(current, temperature, chooseTrialOrCurrent)}
+      (0 until stepsAtEachTemperature).toList.foldLeftM[({type M[B] = State[Random, B]})#M, A](start){case (current, _) => testTrialSolution(current, temperature)}
     }.toList
     val sequenced: State[Random, List[A]] = trialSolutions.sequenceU
     val annealable = implicitly[Annealable[A]]
     sequenced.map(_.find(a => annealable.energy(a) === 0))
   }
 
-  private def trySolution[A: Annealable](current: A, temperature: Temperature, chooseTrialOrCurrent: (A, A, Temperature) => State[Random, A]): State[Random, A] = {
+  private def acceptanceProbability(temperature: Temperature) = (d: Double) => math.exp(-d / temperature.value)
+
+  private def testTrialSolution[A: Annealable](current: A, temperature: Temperature): State[Random, A] = {
     val annealable = implicitly[Annealable[A]]
     val trialSolution = annealable.heat(current)
-    trialSolution.flatMap(ts => chooseTrialOrCurrent(ts, current, temperature))
+    for {
+      ts <- trialSolution
+      threshold <- Random.nextDouble
+    } yield {
+      choose(ts, current, acceptanceProbability(temperature), threshold, annealable.energy _)
+    }
   }
 
   def choose[A](trialSolution: A, current: A, acceptanceProbability: Double => Double, threshold: => Double, energy: A => Double): A = {
